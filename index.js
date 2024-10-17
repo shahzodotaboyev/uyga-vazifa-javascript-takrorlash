9b78c4776426837790103228f62095cbafb9fb10
shahzodotaboyev <shoxzodbek15@gmail.com>
2024-10-17 13:52:21 +0500

    5ta masala

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
 // #uyga vazifa takrorlash
-    
+
+// let products = [
+//     {
+//         id: 6,
+//         name: "Smarthpone",
+//         price: 12000,
+//         rating: 4.5,
+//         discount: 20,
+//     },
+//     {
+//         id: 2,
+//         name: "Acer",
+//         price: 10000,
+//         rating: 4.3,
+//         discount: 10,
+//     },
+//     {
+//         id: 1,
+//         name: "Mac book",
+//         price: 17000,
+//         rating: 4.7,
+//         discount: 40,
+//     },
+//     {
+//         id: 4,
+//         name: "HP",
+//         price: 21000,
+//         rating: 4.1,
+//         discount: 30,
+//     },
+//     {
+//         id: 5,
+//         name: "Dell",
+//         price: 35000,
+//         rating: 4.9,
+//         discount: 30,
+//     },
+// ];
+
+
+// 1
+// products.sort((a, b) => b.rating - a.rating);
+
+// console.log(products);
+
+// 2
+// console.log(products.reduce((a, b) => a + b.price,0));
+
+// 4
+
+// console.log(products.find(products =>products.id == 5));
+
+// 5 (Siz aytgandaka qilipay ishlay olmadim)
+// let matn = 'George Raymond Richard Martin';
+// console.log(matn.match(/[A-Z]/g));
+
+
